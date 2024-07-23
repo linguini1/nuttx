a24d75a4bb2a547d1f4a82423a9c49923cfd26dc
Matteo Golin <matteo.golin@gmail.com>
2024-07-23 09:04:01 -0400

    Moved all files to hardware directory since they are register addr headers. Added a file for base addresses and memory map.

---
+++ arch/arm64/src/bcm2711/hardware/bcm2711_bsc.h
@@ -0,0 +1,92 @@
+/****************************************************************************
+ * arch/arm64/src/bcm2711/bcm2711_bsc.h
+ *
+ * Author: Matteo Golin <matteo.golin@gmail.com>
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_ARM64_SRC_BCM2711_BSC_H
+#define __ARCH_ARM64_SRC_BCM2711_BSC_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* BSC interface addresses */
+
+#define BCM_BSC0 0x7e205000
+#define BCM_BSC1 0x7e804000
+#define BCM_BSC3 0x7e205600
+#define BCM_BSC4 0x7e205800
+#define BCM_BSC5 0x7e205a80
+#define BCM_BSC6 0x7e205c00
+
+/* BSC register offsets */
+
+#define BCM_BSC_C_OFFSET 0x00    /* Control */
+#define BCM_BSC_S_OFFSET 0x04    /* Status */
+#define BCM_BSC_DLEN_OFFSET 0x08 /* Data Length */
+#define BCM_BSC_A_OFFSET 0x0c    /* Slave Address */
+#define BCM_BSC_FIFO_OFFSET 0x10 /* Data FIFO */
+#define BCM_BSC_DIV_OFFSET 0x14  /* Clock Divider */
+#define BCM_BSC_DEL_OFFSET 0x18  /* Data Delay */
+#define BCM_BSC_CLKT_OFFSET 0x1c /* Clock Stretch Timeout */
+
+/* BSC register bit definitions */
+
+#define BCM_BSC_C_I2CEN (1 << 15)   /* Enable I2C */
+#define BCM_BSC_C_INTR (1 << 10)    /* Interrupt on RX */
+#define BCM_BSC_C_INTT (1 << 9)     /* Interrupt on TX */
+#define BCM_BSC_C_INTD (1 << 8)     /* Interrupt on DONE */
+#define BCM_BSC_C_ST (1 << 7)       /* Start transfer */
+#define BCM_BSC_C_CLRMSK (0x3 << 3) /* FIFO clear mask */
+#define BCM_BSC_C_NOCLR (0 << 3)    /* No clear action */
+#define BCM_BSC_C_CLRFIFO (1 << 3)  /* Clear FIFO one shot */
+#define BCM_BSC_C_READ (1 << 0)     /* Read = 1, Write = 0 */
+
+#define BCM_BSC_S_CLKT (1 << 9) /* CLK stretch detected */
+#define BCM_BSC_S_ERR (1 << 8)  /* ACK error */
+#define BCM_BSC_S_RXF (1 << 7)  /* FIFO full */
+#define BCM_BSC_S_TXE (1 << 6)  /* FIFO empty */
+#define BCM_BSC_S_RXD (1 << 5)  /* FIFO has data */
+#define BCM_BSC_S_TXD (1 << 4)  /* FIFO can accept data */
+#define BCM_BSC_S_RXR (1 << 3)  /* FIFO needs reading */
+#define BCM_BSC_S_TXW (1 << 2)  /* FIFO needs writing */
+#define BCM_BSC_S_DONE (1 << 1) /* Transfer done */
+#define BCM_BSC_S_TA (1 << 0)   /* Transfer active */
+
+#define BCM_BSC_DLEN_MASK (0xffff) /* Data length mask */
+
+#define BCM_BSC_A_ADDR (0x7f) /* Slave address */
+
+#define BCM_BSC_FIFO_DATA (0xff) /* FIFO data (RW) */
+
+#define BCM_BSC_DIV_CDIV (0xffff) /* Clock divider */
+
+#define BCM_BSC_DEL_FEDL (0xffff << 16) /* Falling edge delay */
+#define BCM_BSC_DEL_REDL (0xffff)       /* Rising edge delay */
+
+#define BCM_BSC_CLKT_TOUT (0xffff) /* Clock stretch timeout value */
+
+#endif // __ARCH_ARM64_SRC_BCM2711_BSC_H
---
+++ arch/arm64/src/bcm2711/hardware/bcm2711_gpio.h
@@ -0,0 +1,257 @@
+/****************************************************************************
+ * arch/arm64/src/bcm2711/bcm2711_gpio.h
+ *
+ * Author: Matteo Golin <matteo.golin@gmail.com>
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_ARM64_SRC_BCM2711_GPIO_H
+#define __ARCH_ARM64_SRC_BCM2711_GPIO_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* GPIO register offset definitions */
+
+#define BCM_GPIO_BASE_ADDR 0x7e200000
+
+#define BCM_GPIO_GPFSEL0_OFFSET 0x00 /* GPIO function select 0 */
+#define BCM_GPIO_GPFSEL1_OFFSET 0x04 /* GPIO function select 1 */
+#define BCM_GPIO_GPFSEL2_OFFSET 0x08 /* GPIO function select 2 */
+#define BCM_GPIO_GPFSEL3_OFFSET 0x0c /* GPIO function select 3 */
+#define BCM_GPIO_GPFSEL4_OFFSET 0x10 /* GPIO function select 4 */
+#define BCM_GPIO_GPFSEL5_OFFSET 0x14 /* GPIO function select 5 */
+#define BCM_GPIO_GPSET0_OFFSET 0x1c  /* GPIO pin output set 0 */
+#define BCM_GPIO_GPSET1_OFFSET 0x20  /* GPIO pin output set 1 */
+#define BCM_GPIO_GPCLR0_OFFSET 0x28  /* GPIO pin output clear 0 */
+#define BCM_GPIO_GPCLR1_OFFSET 0x2c  /* GPIO pin output clear 1 */
+#define BCM_GPIO_GPLEV0_OFFSET 0x34  /* GPIO pin level 0 */
+#define BCM_GPIO_GPLEV1_OFFSET 0x38  /* GPIO pin level 1 */
+
+#define BCM_GPIO_GPEDS0_OFFSET 0x40  /* GPIO pin event detect status 0 */
+#define BCM_GPIO_GPEDS1_OFFSET 0x44  /* GPIO pin event detect status 1 */
+#define BCM_GPIO_GPREN0_OFFSET 0x4c  /* GPIO pin rise edge detect enable 0 */
+#define BCM_GPIO_GPREN1_OFFSET 0x50  /* GPIO pin rise edge detect enable 1 */
+#define BCM_GPIO_GPFEN0_OFFSET 0x58  /* GPIO pin fall edge detect enable 0 */
+#define BCM_GPIO_GPFEN1_OFFSET 0x5c  /* GPIO pin fall edge detect enable 1 */
+#define BCM_GPIO_GPHEN0_OFFSET 0x64  /* GPIO pin high detect enable 0 */
+#define BCM_GPIO_GPHEN1_OFFSET 0x68  /* GPIO pin high detect enable 1 */
+#define BCM_GPIO_GPLEN0_OFFSET 0x70  /* GPIO pin low detect enable 0 */
+#define BCM_GPIO_GPLEN1_OFFSET 0x74  /* GPIO pin low detect enable 1 */
+#define BCM_GPIO_GPAREN0_OFFSET 0x7c /* GPIO pin async rise edge detect 0 */
+#define BCM_GPIO_GPAREN1_OFFSET 0x80 /* GPIO pin async rise edge detect 1 */
+#define BCM_GPIO_GPAFEN0_OFFSET 0x88 /* GPIO pin async fall edge detect 0 */
+#define BCM_GPIO_GPAFEN1_OFFSET 0x8c /* GPIO pin async fall edge detect 1 */
+
+#define BCM_GPIO_PUP_PDN_REG0_OFFSET 0xe4 /* GPIO pullup/down reg 0 */
+#define BCM_GPIO_PUP_PDN_REG1_OFFSET 0xe8 /* GPIO pullup/down reg 1 */
+#define BCM_GPIO_PUP_PDN_REG2_OFFSET 0xec /* GPIO pullup/down reg 2 */
+#define BCM_GPIO_PUP_PDN_REG3_OFFSET 0xf0 /* GPIO pullup/down reg 3 */
+
+/* GPIO register address definitions */
+
+#define _BCM_GP_REG(reg) (BCM_GPIO_BASE_ADDR + (reg))
+
+#define BCM_GPIO_GPFSEL0 _BCM_GP_REG(BCM_GPIO_GPFSEL0_OFFSET)
+#define BCM_GPIO_GPFSEL1 _BCM_GP_REG(BCM_GPIO_GPFSEL1_OFFSET)
+#define BCM_GPIO_GPFSEL2 _BCM_GP_REG(BCM_GPIO_GPFSEL2_OFFSET)
+#define BCM_GPIO_GPFSEL3 _BCM_GP_REG(BCM_GPIO_GPFSEL3_OFFSET)
+#define BCM_GPIO_GPFSEL4 _BCM_GP_REG(BCM_GPIO_GPFSEL4_OFFSET)
+#define BCM_GPIO_GPFSEL5 _BCM_GP_REG(BCM_GPIO_GPFSEL5_OFFSET)
+#define BCM_GPIO_GPSET0 _BCM_GP_REG(BCM_GPIO_GPSET0_OFFSET)
+#define BCM_GPIO_GPSET1 _BCM_GP_REG(BCM_GPIO_GPSET1_OFFSET)
+#define BCM_GPIO_GPCLR0 _BCM_GP_REG(BCM_GPIO_GPCLR0_OFFSET)
+#define BCM_GPIO_GPCLR1 _BCM_GP_REG(BCM_GPIO_GPCLR1_OFFSET)
+#define BCM_GPIO_GPLEV0 _BCM_GP_REG(BCM_GPIO_GPLEV0_OFFSET)
+#define BCM_GPIO_GPLEV1 _BCM_GP_REG(BCM_GPIO_GPLEV1_OFFSET)
+#define BCM_GPIO_GPEDS0 _BCM_GP_REG(BCM_GPIO_GPEDS0_OFFSET)
+#define BCM_GPIO_GPEDS1 _BCM_GP_REG(BCM_GPIO_GPEDS1_OFFSET)
+#define BCM_GPIO_GPREN0 _BCM_GP_REG(BCM_GPIO_GPREN0_OFFSET)
+#define BCM_GPIO_GPREN1 _BCM_GP_REG(BCM_GPIO_GPREN1_OFFSET)
+#define BCM_GPIO_GPFEN0 _BCM_GP_REG(BCM_GPIO_GPFEN0_OFFSET)
+#define BCM_GPIO_GPFEN1 _BCM_GP_REG(BCM_GPIO_GPFEN1_OFFSET)
+#define BCM_GPIO_GPHEN0 _BCM_GP_REG(BCM_GPIO_GPHEN0_OFFSET)
+#define BCM_GPIO_GPHEN1 _BCM_GP_REG(BCM_GPIO_GPHEN1_OFFSET)
+#define BCM_GPIO_GPLEN0 _BCM_GP_REG(BCM_GPIO_GPLEN0_OFFSET)
+#define BCM_GPIO_GPLEN1 _BCM_GP_REG(BCM_GPIO_GPLEN1_OFFSET)
+#define BCM_GPIO_GPAREN0 _BCM_GP_REG(BCM_GPIO_GPAREN0_OFFSET)
+#define BCM_GPIO_GPAREN1 _BCM_GP_REG(BCM_GPIO_GPAREN1_OFFSET)
+#define BCM_GPIO_GPAFEN0 _BCM_GP_REG(BCM_GPIO_GPAFEN0_OFFSET)
+#define BCM_GPIO_GPAFEN1 _BCM_GP_REG(BCM_GPIO_GPAFEN1_OFFSET)
+#define BCM_GPIO_PUP_PDN_CNTRL_REG0 _BCM_GP_REG(BCM_GPIO_PUP_PDN_REG0_OFFSET)
+#define BCM_GPIO_PUP_PDN_CNTRL_REG1 _BCM_GP_REG(BCM_GPIO_PUP_PDN_REG1_OFFSET)
+#define BCM_GPIO_PUP_PDN_CNTRL_REG2 _BCM_GP_REG(BCM_GPIO_PUP_PDN_REG2_OFFSET)
+#define BCM_GPIO_PUP_PDN_CNTRL_REG3 _BCM_GP_REG(BCM_GPIO_PUP_PDN_REG3_OFFSET)
+
+/* GPIO register bit definitions */
+
+#define BCM_GPIO_FS_IN 0x0   /* Pin is input */
+#define BCM_GPIO_FS_OUT 0x1  /* Pin is output */
+#define BCM_GPIO_FS_ALT0 0x4 /* Pin is alternate func 0 */
+#define BCM_GPIO_FS_ALT1 0x5 /* Pin is alternate func 1 */
+#define BCM_GPIO_FS_ALT2 0x6 /* Pin is alternate func 2 */
+#define BCM_GPIO_FS_ALT3 0x7 /* Pin is alternate func 3 */
+#define BCM_GPIO_FS_ALT4 0x3 /* Pin is alternate func 4 */
+#define BCM_GPIO_FS_ALT5 0x2 /* Pin is alternate func 5 */
+
+#define BCM_GPIO_GPFSEL0_FSEL9 (0x7 << 27)
+#define BCM_GPIO_GPFSEL0_FSEL8 (0x7 << 24)
+#define BCM_GPIO_GPFSEL0_FSEL7 (0x7 << 21)
+#define BCM_GPIO_GPFSEL0_FSEL6 (0x7 << 18)
+#define BCM_GPIO_GPFSEL0_FSEL5 (0x7 << 15)
+#define BCM_GPIO_GPFSEL0_FSEL4 (0x7 << 12)
+#define BCM_GPIO_GPFSEL0_FSEL3 (0x7 << 9)
+#define BCM_GPIO_GPFSEL0_FSEL2 (0x7 << 6)
+#define BCM_GPIO_GPFSEL0_FSEL1 (0x7 << 3)
+#define BCM_GPIO_GPFSEL0_FSEL0 (0x7 << 0)
+
+#define BCM_GPIO_GPFSEL1_FSEL19 (0x7 << 27)
+#define BCM_GPIO_GPFSEL1_FSEL18 (0x7 << 24)
+#define BCM_GPIO_GPFSEL1_FSEL17 (0x7 << 21)
+#define BCM_GPIO_GPFSEL1_FSEL16 (0x7 << 18)
+#define BCM_GPIO_GPFSEL1_FSEL15 (0x7 << 15)
+#define BCM_GPIO_GPFSEL1_FSEL14 (0x7 << 12)
+#define BCM_GPIO_GPFSEL1_FSEL13 (0x7 << 9)
+#define BCM_GPIO_GPFSEL1_FSEL12 (0x7 << 6)
+#define BCM_GPIO_GPFSEL1_FSEL11 (0x7 << 3)
+#define BCM_GPIO_GPFSEL1_FSEL10 (0x7 << 0)
+
+#define BCM_GPIO_GPFSEL2_FSEL29 (0x7 << 27)
+#define BCM_GPIO_GPFSEL2_FSEL28 (0x7 << 24)
+#define BCM_GPIO_GPFSEL2_FSEL27 (0x7 << 21)
+#define BCM_GPIO_GPFSEL2_FSEL26 (0x7 << 18)
+#define BCM_GPIO_GPFSEL2_FSEL25 (0x7 << 15)
+#define BCM_GPIO_GPFSEL2_FSEL24 (0x7 << 12)
+#define BCM_GPIO_GPFSEL2_FSEL23 (0x7 << 9)
+#define BCM_GPIO_GPFSEL2_FSEL22 (0x7 << 6)
+#define BCM_GPIO_GPFSEL2_FSEL21 (0x7 << 3)
+#define BCM_GPIO_GPFSEL2_FSEL20 (0x7 << 0)
+
+#define BCM_GPIO_GPFSEL3_FSEL39 (0x7 << 27)
+#define BCM_GPIO_GPFSEL3_FSEL38 (0x7 << 24)
+#define BCM_GPIO_GPFSEL3_FSEL37 (0x7 << 21)
+#define BCM_GPIO_GPFSEL3_FSEL36 (0x7 << 18)
+#define BCM_GPIO_GPFSEL3_FSEL35 (0x7 << 15)
+#define BCM_GPIO_GPFSEL3_FSEL34 (0x7 << 12)
+#define BCM_GPIO_GPFSEL3_FSEL33 (0x7 << 9)
+#define BCM_GPIO_GPFSEL3_FSEL32 (0x7 << 6)
+#define BCM_GPIO_GPFSEL3_FSEL31 (0x7 << 3)
+#define BCM_GPIO_GPFSEL3_FSEL30 (0x7 << 0)
+
+#define BCM_GPIO_GPFSEL4_FSEL49 (0x7 << 27)
+#define BCM_GPIO_GPFSEL4_FSEL48 (0x7 << 24)
+#define BCM_GPIO_GPFSEL4_FSEL47 (0x7 << 21)
+#define BCM_GPIO_GPFSEL4_FSEL46 (0x7 << 18)
+#define BCM_GPIO_GPFSEL4_FSEL45 (0x7 << 15)
+#define BCM_GPIO_GPFSEL4_FSEL44 (0x7 << 12)
+#define BCM_GPIO_GPFSEL4_FSEL43 (0x7 << 9)
+#define BCM_GPIO_GPFSEL4_FSEL42 (0x7 << 6)
+#define BCM_GPIO_GPFSEL4_FSEL41 (0x7 << 3)
+#define BCM_GPIO_GPFSEL4_FSEL40 (0x7 << 0)
+
+#define BCM_GPIO_GPFSEL5_FSEL57 (0x7 << 21)
+#define BCM_GPIO_GPFSEL5_FSEL56 (0x7 << 18)
+#define BCM_GPIO_GPFSEL5_FSEL55 (0x7 << 15)
+#define BCM_GPIO_GPFSEL5_FSEL54 (0x7 << 12)
+#define BCM_GPIO_GPFSEL5_FSEL53 (0x7 << 9)
+#define BCM_GPIO_GPFSEL5_FSEL52 (0x7 << 6)
+#define BCM_GPIO_GPFSEL5_FSEL51 (0x7 << 3)
+#define BCM_GPIO_GPFSEL5_FSEL50 (0x7 << 0)
+
+/* Any register that does not have specific bit masks listed here is omitted
+ * because the control is done via a single bit, which corresponds bit N to
+ * pin N. It is up to the programmer to read the data sheet to know whether
+ * to use register 0, 1, ..., n.
+ */
+
+#define BCM_GPIO_NORES 0x0    /* No resistor */
+#define BCM_GPIO_PULLUP 0x1   /* Pull-up resistor */
+#define BCM_GPIO_PULLDOWN 0x2 /* Pull-down resistor */
+
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL15 (0x3 << 30)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL14 (0x3 << 28)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL13 (0x3 << 26)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL12 (0x3 << 24)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL11 (0x3 << 22)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL10 (0x3 << 20)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL9 (0x3 << 18)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL8 (0x3 << 16)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL7 (0x3 << 14)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL6 (0x3 << 12)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL5 (0x3 << 10)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL4 (0x3 << 8)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL3 (0x3 << 6)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL2 (0x3 << 4)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL1 (0x3 << 2)
+#define BCM_GPIO_PUP_PDN_REG0_CNTRL0 (0x3 << 1)
+
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL31 (0x3 << 30)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL30 (0x3 << 28)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL29 (0x3 << 26)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL28 (0x3 << 24)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL27 (0x3 << 22)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL26 (0x3 << 20)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL25 (0x3 << 18)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL24 (0x3 << 16)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL23 (0x3 << 14)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL22 (0x3 << 12)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL21 (0x3 << 10)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL20 (0x3 << 8)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL19 (0x3 << 6)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL18 (0x3 << 4)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL17 (0x3 << 2)
+#define BCM_GPIO_PUP_PDN_REG1_CNTRL16 (0x3 << 1)
+
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL47 (0x3 << 30)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL46 (0x3 << 28)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL45 (0x3 << 26)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL44 (0x3 << 24)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL43 (0x3 << 22)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL42 (0x3 << 20)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL41 (0x3 << 18)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL40 (0x3 << 16)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL39 (0x3 << 14)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL38 (0x3 << 12)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL37 (0x3 << 10)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL36 (0x3 << 8)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL35 (0x3 << 6)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL34 (0x3 << 4)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL33 (0x3 << 2)
+#define BCM_GPIO_PUP_PDN_REG2_CNTRL32 (0x3 << 1)
+
+#define BCM_GPIO_PUP_PDN_REG3_CNTRL57 (0x3 << 18)
+#define BCM_GPIO_PUP_PDN_REG3_CNTRL56 (0x3 << 16)
+#define BCM_GPIO_PUP_PDN_REG3_CNTRL55 (0x3 << 14)
+#define BCM_GPIO_PUP_PDN_REG3_CNTRL54 (0x3 << 12)
+#define BCM_GPIO_PUP_PDN_REG3_CNTRL53 (0x3 << 10)
+#define BCM_GPIO_PUP_PDN_REG3_CNTRL52 (0x3 << 8)
+#define BCM_GPIO_PUP_PDN_REG3_CNTRL51 (0x3 << 6)
+#define BCM_GPIO_PUP_PDN_REG3_CNTRL50 (0x3 << 4)
+#define BCM_GPIO_PUP_PDN_REG3_CNTRL49 (0x3 << 2)
+#define BCM_GPIO_PUP_PDN_REG3_CNTRL48 (0x3 << 1)
+
+/* TODO: how to encode alternative function table to preproc definitions? */
+
+#endif // __ARCH_ARM64_SRC_BCM2711_GPIO_H
