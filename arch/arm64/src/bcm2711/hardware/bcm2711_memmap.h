c5437eb4ea41a440c53c0626076ec4adaf15d986
Matteo Golin <matteo.golin@gmail.com>
2024-07-29 12:54:53 -0400

    Implement peripheral base address changes for low peripheral and legacy addressing modes.

--- arch/arm64/src/bcm2711/hardware/bcm2711_memmap.h
+++ arch/arm64/src/bcm2711/hardware/bcm2711_memmap.h
@@ -33,44 +33,88 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
+/* BCM2711 peripheral base address */
+
+#if defined(CONFIG_BCM2711_LOW_PERIPHERAL)
+
+/* Low peripheral addressing mode */
+
+#define BCM_PERIPHERAL_BASEADDR 0x0fe000000
+
+#elif defined(CONFIG_BCM2711_LEGACY_ADDR)
+
+/* Legacy addressing mode */
+
+#define BCM_PERIPHERAL_BASEADDR 0x07e000000
+
+#else
+
+/* 35-bit addressing mode */
+
+#define BCM_PERIPHERAL_BASEADDR 0x47e000000
+
+#endif // defined(CONFIG_BCM2711_LOW_PERIPHERAL)
+
 /* Base addresses for chip registers */
 
-#define BCM_ARMT_BASEADDR 0x7e00b000  /* ARM timer */
-#define BCM_AUX_BASEADDR 0x7e215000   /* Auxilliary */
-#define BCM_GPCLK_BASEADDR 0x7e101000 /* General purpose clock */
-#define BCM_GPIO_BASEADDR 0x7e200000  /* GPIO */
-#define BCM_PCM_BASEADDR 0x7e203000   /* PCM */
-#define BCM_SYST_BASEADDR 0x7e003000  /* System timer */
+#define BCM_ARMT_BASEADDR                                                    \
+  (BCM_PERIPHERAL_BASEADDR + 0x0000b000) /* ARM timer */
+#define BCM_AUX_BASEADDR                                                     \
+  (BCM_PERIPHERAL_BASEADDR + 0x000215000) /* Auxilliary */
+#define BCM_GPCLK_BASEADDR                                                   \
+  (BCM_PERIPHERAL_BASEADDR + 0x000101000) /* General purpose clock */
+#define BCM_GPIO_BASEADDR (BCM_PERIPHERAL_BASEADDR + 0x000200000) /* GPIO */
+#define BCM_PCM_BASEADDR (BCM_PERIPHERAL_BASEADDR + 0x000203000)  /* PCM */
+#define BCM_SYST_BASEADDR                                                    \
+  (BCM_PERIPHERAL_BASEADDR + 0x000003000) /* System timer */
 
 /* BSC interface base addresses */
 
-#define BCM_BSC0 0x7e205000 /* BSC/I2C interface 0 */
-#define BCM_BSC1 0x7e804000 /* BSC/I2C interface 1 */
-#define BCM_BSC3 0x7e205600 /* BSC/I2C interface 2 */
-#define BCM_BSC4 0x7e205800 /* BSC/I2C interface 3 */
-#define BCM_BSC5 0x7e205a80 /* BSC/I2C interface 4 */
-#define BCM_BSC6 0x7e205c00 /* BSC/I2C interface 5 */
+#define BCM_BSC0                                                             \
+  (BCM_PERIPHERAL_BASEADDR + 0x000205000) /* BSC/I2C interface 0 */
+#define BCM_BSC1                                                             \
+  (BCM_PERIPHERAL_BASEADDR + 0x000804000) /* BSC/I2C interface 1 */
+#define BCM_BSC3                                                             \
+  (BCM_PERIPHERAL_BASEADDR + 0x000205600) /* BSC/I2C interface 2 */
+#define BCM_BSC4                                                             \
+  (BCM_PERIPHERAL_BASEADDR + 0x000205800) /* BSC/I2C interface 3 */
+#define BCM_BSC5                                                             \
+  (BCM_PERIPHERAL_BASEADDR + 0x000205a80) /* BSC/I2C interface 4 */
+#define BCM_BSC6                                                             \
+  (BCM_PERIPHERAL_BASEADDR + 0x000205c00) /* BSC/I2C interface 5 */
 
 /* SPI interface register base addresses */
 
-#define BCM_SPI0_BASEADDR 0x7e204000 /* SPI interface 0 */
-#define BCM_SPI3_BASEADDR 0x7e204600 /* SPI interface 3 */
-#define BCM_SPI4_BASEADDR 0x7e204800 /* SPI interface 4 */
-#define BCM_SPI5_BASEADDR 0x7e204a00 /* SPI interface 5 */
-#define BCM_SPI6_BASEADDR 0x7e204c00 /* SPI interface 6 */
+#define BCM_SPI0_BASEADDR                                                    \
+  (BCM_PERIPHERAL_BASEADDR + 0x000204000) /* SPI interface 0 */
+#define BCM_SPI3_BASEADDR                                                    \
+  (BCM_PERIPHERAL_BASEADDR + 0x000204600) /* SPI interface 3 */
+#define BCM_SPI4_BASEADDR                                                    \
+  (BCM_PERIPHERAL_BASEADDR + 0x000204800) /* SPI interface 4 */
+#define BCM_SPI5_BASEADDR                                                    \
+  (BCM_PERIPHERAL_BASEADDR + 0x000204a00) /* SPI interface 5 */
+#define BCM_SPI6_BASEADDR                                                    \
+  (BCM_PERIPHERAL_BASEADDR + 0x000204c00) /* SPI interface 6 */
 
 /* UART interface base addresses */
 
-#define BCM_UART0_BASEADDR 0x7e201000 /* UART interface 0 */
-#define BCM_UART2_BASEADDR 0x7e201400 /* UART interface 2 */
-#define BCM_UART3_BASEADDR 0x7e201600 /* UART interface 3 */
-#define BCM_UART4_BASEADDR 0x7e201800 /* UART interface 4 */
-#define BCM_UART5_BASEADDR 0x7e201a00 /* UART interface 5 */
+#define BCM_UART0_BASEADDR                                                   \
+  (BCM_PERIPHERAL_BASEADDR + 0x000201000) /* UART interface 0 */
+#define BCM_UART2_BASEADDR                                                   \
+  (BCM_PERIPHERAL_BASEADDR + 0x000201400) /* UART interface 2 */
+#define BCM_UART3_BASEADDR                                                   \
+  (BCM_PERIPHERAL_BASEADDR + 0x000201600) /* UART interface 3 */
+#define BCM_UART4_BASEADDR                                                   \
+  (BCM_PERIPHERAL_BASEADDR + 0x000201800) /* UART interface 4 */
+#define BCM_UART5_BASEADDR                                                   \
+  (BCM_PERIPHERAL_BASEADDR + 0x000201a00) /* UART interface 5 */
 
 /* DMA channel base addresses */
 
-#define BCM_DMA0_BASE 0x7e007000  /* DMA Channel 0 */
-#define BCM_DMA15_BASE 0x7ee05000 /* DMA Channel 15 */
+#define BCM_DMA0_BASE                                                        \
+  (BCM_PERIPHERAL_BASEADDR + 0x000007000) /* DMA Channel 0 */
+#define BCM_DMA15_BASE                                                       \
+  (BCM_PERIPHERAL_BASEADDR + 0x000e05000) /* DMA Channel 15 */
 
 /* ARM_LOCAL base address */
 
